afc67c9c1a4694a1a86f95dab995b7d686e1c9af
Kyro <mariogallardo1331@yahoo.com>
2022-03-19 16:25:01 -0500

    adding reply feature to comments

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -22,6 +22,24 @@ const commentController = {
         .catch(err => res.json(err));
     },
 
+    // adding reply
+    addReply({ params, body }, res) {
+        console.log(body);
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    },
+
     // remove comment
     removeComment({ params }, res) {
         Comment.findOneAndDelete({ _id: params.commentId })
@@ -43,6 +61,17 @@ const commentController = {
             res.json(dbPizzaData);
         })
         .catch(err => res.json(err));
+    },
+
+    // remove reply
+    removeReply({ params}, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.json(err))
     }
 };
 
